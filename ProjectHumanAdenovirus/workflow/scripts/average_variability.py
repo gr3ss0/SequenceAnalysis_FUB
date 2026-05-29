acbfbd975870f32a9d667925b060c269edca62e3
Michal Gress <michag01@zedat.fu-berlin.de>
2026-05-30 00:07:13 +0200

    print  stderr to logfile

---
+++ ProjectHumanAdenovirus/workflow/scripts/average_variability.py
@@ -0,0 +1,18 @@
+import numpy as np
+import sys
+
+# Redirect standard output and error to the log file defined in the Snakemake rule
+sys.stderr = sys.stdout = open(snakemake.log[0], "w")
+
+WINDOW_SIZE = int(snakemake.params.window_size)
+variability = np.loadtxt(snakemake.input.variability, delimiter=",", dtype=float)
+
+average_variability = []
+
+for i in range(0,len(variability),WINDOW_SIZE):
+    window_variability = variability[i:i+WINDOW_SIZE]
+    average_variability.append(np.mean(window_variability))
+    print(f"Window {i//WINDOW_SIZE + 1}: Average Variability = {average_variability[-1]}")
+
+np.savetxt(snakemake.output.variability, average_variability, fmt="%.6f", delimiter=",")
+
---
+++ ProjectHumanAdenovirus/workflow/scripts/calculate_variability.py
@@ -0,0 +1,35 @@
+from Bio import AlignIO
+import numpy as np
+import sys
+
+# Redirect standard output and error to the log file defined in the Snakemake rule
+sys.stderr = sys.stdout = open(snakemake.log[0], "w")
+
+MSA = AlignIO.read(snakemake.input.msa, "fasta")
+
+
+def shannon_entropy(column):
+    from collections import Counter
+    import math
+    
+    counts = Counter(column)
+    total = sum(counts.values())
+    
+    entropy = 0.0
+    for count in counts.values():
+        p = count / total
+        entropy -= p * math.log2(p)
+    
+    return entropy
+
+
+
+entropies = []
+for i in range(MSA.get_alignment_length()):
+    column = MSA[:, i]
+    variability = shannon_entropy(column)
+    entropies.append(variability)
+    print(f"Position {i+1}: Variability = {variability}")
+
+# Save the variability values to a text file
+np.savetxt(snakemake.output.variability, entropies, fmt="%.6f", delimiter=",")
